a47a5690ec03f4fe6ec669cd2b71a3b7c5f45555
devin <hi@devin.sg>
2024-09-25 07:55:04 -0700

    Make modal store logic

--- src/store/handleModal.js
+++ src/store/handleModal.js
@@ -2,9 +2,18 @@ import { atom } from "nanostores";
 
 const initialIsOpen = true
 const isOpen = atom(initialIsOpen)
+const initialContent = {}
+const storedContent = atom(initialContent)
 
-const closeModal = () => {	
+const toggleModal = () => {	
 	isOpen.set(!isOpen.value)
 }
 
-export { isOpen, closeModal }
+const updateContent = ( original ) => {
+	console.log( "debug content update" )
+	console.log( "original content, " + original.title )
+	storedContent.set(original)
+	console.log(storedContent.get().title)
+}
+
+export { isOpen, toggleModal, storedContent, updateContent }
